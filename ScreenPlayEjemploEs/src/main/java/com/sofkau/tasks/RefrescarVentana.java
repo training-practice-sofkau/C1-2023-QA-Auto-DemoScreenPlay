553f9116fd2e35495c9f589c726210a86905abc1
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2023-03-22 00:45:25 -0500

    Refrescar Ventana

--- ScreenPlayEjemploEs/src/main/java/com/sofkau/tasks/RefrescarVentana.java
+++ ScreenPlayEjemploEs/src/main/java/com/sofkau/tasks/RefrescarVentana.java
@@ -1,2 +1,30 @@
-package com.sofkau.tasks;public class RefrescarVentana {
+package com.sofkau.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.actions.*;
+import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.WebDriver;
+
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class RefrescarVentana implements Task {
+
+    public RefrescarVentana() { }
+
+    public static RefrescarVentana thePage() {
+        return Tasks.instrumented(RefrescarVentana.class);
+    }
+
+    @Override
+    @Step("{0} refreshes the browser")
+    public <T extends Actor> void performAs(T actor) {
+
+        WebDriver driver = BrowseTheWeb.as(actor).getDriver();
+        driver.navigate().refresh();
+    }
 }
