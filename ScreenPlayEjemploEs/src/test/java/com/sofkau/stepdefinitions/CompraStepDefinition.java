04812cb6c0d31d2c3a736fed61845553fa08edc9
nunezjose770 <nunezjose770@gmail.com>
2023-03-24 16:38:41 -0500

    Se agregan los respectivos try-catch y loggers

--- ScreenPlayEjemploEs/src/test/java/com/sofkau/stepdefinitions/CompraStepDefinition.java
+++ ScreenPlayEjemploEs/src/test/java/com/sofkau/stepdefinitions/CompraStepDefinition.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import com.sofkau.setup.Configuracion;
 import io.cucumber.java.ast.Cuando;
 import io.cucumber.java.es.Entonces;
+import org.apache.log4j.Logger;
 
 import static com.sofkau.questions.MensajeCompra.mensajeCompra;
 import static com.sofkau.tasks.BuscarProductos1.buscarProductos1;
@@ -18,35 +19,51 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class CompraStepDefinition extends Configuracion {
 
     Faker faker = new Faker();
+    public static Logger LOGGER= Logger.getLogger(CompraStepDefinition.class);
 
     @Cuando("Inicio sesion en la pagina")
     public void inicioSesionEnLaPagina() {
-        theActorInTheSpotlight().attemptsTo(
-                iniciarSesion()
-                        .conElUsuario("123@gmail.com")
-                        .yConLaContrasenna("123456")
-        );
+        try {
+            theActorInTheSpotlight().attemptsTo(
+                    iniciarSesion()
+                            .conElUsuario("123@gmail.com")
+                            .yConLaContrasenna("123456")
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Cuando("Seleccione y pague un producto")
     public void seleccioneYPagueUnProducto() {
-        theActorInTheSpotlight().attemptsTo(
-                buscarProductos1(),
-                refrescarPagina(),
-                buscarProductos2(),
-                llenarDatosDePago()
-                        .conElNombre(faker.name().firstName())
-                        .conCardNumber(faker.numerify("#########"))
-                        .conElCVC(faker.numerify("###"))
-                        .conElMes(String.valueOf(faker.number().numberBetween(1,12)))
-                        .conElYear(String.valueOf(faker.number().numberBetween(2023,2030)))
-        );
+        try {
+            theActorInTheSpotlight().attemptsTo(
+                    buscarProductos1(),
+                    refrescarPagina(),
+                    buscarProductos2(),
+                    llenarDatosDePago()
+                            .conElNombre(faker.name().firstName())
+                            .conCardNumber(faker.numerify("#########"))
+                            .conElCVC(faker.numerify("###"))
+                            .conElMes(String.valueOf(faker.number().numberBetween(1, 12)))
+                            .conElYear(String.valueOf(faker.number().numberBetween(2023, 2030)))
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
     @Entonces("el usuario debera ver un mensaje de compra exitosa")
     public void elUsuarioDeberaVerUnMensajeDeCompraExitosa() {
-        theActorInTheSpotlight().should(
-                seeThat(mensajeCompra(), equalTo("ORDER PLACED!"))
-        );
-        quitarDriver();
+        try {
+            theActorInTheSpotlight().should(
+                    seeThat(mensajeCompra(), equalTo("ORDER PLACED!"))
+            );
+            quitarDriver();
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 }
--- ScreenPlayEjemploEs/src/test/java/com/sofkau/stepdefinitions/RegistroInicioSesionStepDefinitions.java
+++ ScreenPlayEjemploEs/src/test/java/com/sofkau/stepdefinitions/RegistroInicioSesionStepDefinitions.java
@@ -6,6 +6,7 @@ import com.sofkau.tasks.AbrirPaginaInicial;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import org.apache.log4j.Logger;
 
 import static com.sofkau.questions.MensajeNombre.mensajeNombre;
 import static com.sofkau.questions.MensajeRegistro.mensajeRegistro;
@@ -19,79 +20,107 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class RegistroInicioSesionStepDefinitions extends Configuracion {
     Faker faker = new Faker();
+    public static Logger LOGGER= Logger.getLogger(RegistroInicioSesionStepDefinitions.class);
     private String nameUser = faker.name().username();
     private String password = faker.numerify("#######");
     private String email = faker.internet().emailAddress();
 
     @Dado("que el usuario esta en la pagina de inicio")
     public void queElUsuarioEstaEnLaPaginaDeInicio() {
-        configurarNavegador();
+        try {
+            configurarNavegador();
 
-        theActorInTheSpotlight().wasAbleTo(
-                new AbrirPaginaInicial()
+            theActorInTheSpotlight().wasAbleTo(
+                    new AbrirPaginaInicial()
 
-        );
-
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Cuando("navega hasta la el formulario de registro")
     public void navegaHastaLaElFormularioDeRegistro() {
-        theActorInTheSpotlight().attemptsTo(
-                navegarAlRegistro()
-        );
+        try {
+            theActorInTheSpotlight().attemptsTo(
+                    navegarAlRegistro()
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Cuando("Llena todos los campos")
     public void llenaTodosLosCampos() {
-        theActorInTheSpotlight().attemptsTo(
-                iniciarRegistro()
-                        .conElUsuario(nameUser)
-                        .yConElEmail(email)
-        );
-        theActorInTheSpotlight().attemptsTo(
-                llenarRegistro()
-                        .conElGenero("Hombre")
-                        .laContrasenna(password)
-                        .elDia(String.valueOf(faker.number().numberBetween(1,28)))
-                        .elMonth(String.valueOf(faker.number().numberBetween(1,12)))
-                        .elYear(String.valueOf(faker.number().numberBetween(1975,2005)))
-                        .elFirstName(faker.name().firstName())
-                        .elLastName(faker.name().lastName())
-                        .laCompany(faker.company().name())
-                        .laAddress(faker.address().streetAddress())
-                        .laAddress2(faker.address().streetAddress())
-                        .elPais("Israel")
-                        .elEstado(faker.country().name())
-                        .laciudad(faker.country().name())
-                        .elZipCode(faker.numerify("######"))
-                        .yElNumeroCelular(faker.numerify("##########"))
-        );
+        try {
+            theActorInTheSpotlight().attemptsTo(
+                    iniciarRegistro()
+                            .conElUsuario(nameUser)
+                            .yConElEmail(email)
+            );
+            theActorInTheSpotlight().attemptsTo(
+                    llenarRegistro()
+                            .conElGenero("Hombre")
+                            .laContrasenna(password)
+                            .elDia(String.valueOf(faker.number().numberBetween(1, 28)))
+                            .elMonth(String.valueOf(faker.number().numberBetween(1, 12)))
+                            .elYear(String.valueOf(faker.number().numberBetween(1975, 2005)))
+                            .elFirstName(faker.name().firstName())
+                            .elLastName(faker.name().lastName())
+                            .laCompany(faker.company().name())
+                            .laAddress(faker.address().streetAddress())
+                            .laAddress2(faker.address().streetAddress())
+                            .elPais("Israel")
+                            .elEstado(faker.country().name())
+                            .laciudad(faker.country().name())
+                            .elZipCode(faker.numerify("######"))
+                            .yElNumeroCelular(faker.numerify("##########"))
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Entonces("el usuario debe ser redireccionado a la pagina principal")
     public void elUsuarioDebeSerRedireccionadoALaPaginaPrincipal() {
-        theActorInTheSpotlight().should(
-                seeThat(mensajeRegistro(), equalTo("ACCOUNT CREATED!"))
-        );
-        quitarDriver();
+        try {
+            theActorInTheSpotlight().should(
+                    seeThat(mensajeRegistro(), equalTo("ACCOUNT CREATED!"))
+            );
+            quitarDriver();
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Cuando("completa los campos para iniciar sesion")
     public void completaLosCamposParaIniciarSesion() {
-        theActorInTheSpotlight().attemptsTo(
-                iniciarSesion()
-                        .conElUsuario("123@gmail.com")
-                        .yConLaContrasenna("123456")
-        );
+        try {
+            theActorInTheSpotlight().attemptsTo(
+                    iniciarSesion()
+                            .conElUsuario("123@gmail.com")
+                            .yConLaContrasenna("123456")
+            );
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
 
     @Entonces("el usuario debe ver su nombre en la pagina principal")
     public void elUsuarioDebeVerSuNombreEnLaPaginaPrincipal() {
-        theActorInTheSpotlight().should(
-                seeThat(mensajeNombre(), equalTo("Logged in as Pruebas123"))
-        );
-        quitarDriver();
+        try {
+            theActorInTheSpotlight().should(
+                    seeThat(mensajeNombre(), equalTo("Logged in as Pruebas123"))
+            );
+            quitarDriver();
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+            quitarDriver();
+        }
     }
-
-
 }
